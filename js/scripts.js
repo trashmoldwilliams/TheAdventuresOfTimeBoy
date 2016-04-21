3df42a5bc0913f2edbbe023ff2c49ff136ee3747
Will Johnson <trashmoldwilliams@gmail.com>
2016-04-21 16:25:51 -0700

    Add clock.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
   Game = function() {
-    this.timeRemaining = 300;
+    //Rendering
     this.fps = 30;
+
+    //Clock
+    this.currentTime = 48;
+    this.flavorHour = 7;
+    this.flavorMinute = 2;
+
+    //Doomsday Timer
+    this.timeRemaining = 192;
     this.countdownInterval = 0;
   }
 
@@ -11,7 +19,25 @@ $(document).ready(function() {
     } else {
       game.countdownInterval = 0;
       game.timeRemaining -= 1;
+      game.currentTime += 1;
     }
+
+    //Clock
+    var currentMinute = game.flavorMinute + Math.floor(game.currentTime / 60);
+    var currentSecond = game.currentTime - (Math.floor(game.currentTime / 60) * 60);
+
+    $("#currentHour").text(game.flavorHour);
+    $("#currentMinute").text("0");
+    $("#currentMinute").append(currentMinute);
+    if(currentSecond <= 9) {
+      $("#currentSecond").text("0");
+      $("#currentSecond").append(currentSecond);
+    } else {
+      $("#currentSecond").text(currentSecond);
+    }
+
+
+    //Doomsday Timer
     var minutesRemaining = Math.floor(game.timeRemaining / 60);
     var secondsRemaining = game.timeRemaining - (minutesRemaining * 60);
 
